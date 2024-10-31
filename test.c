05e471ebafea6317350820cf749520ea45561733
doudoumai007 <lipeiyao_@outlook.com>
2024-10-31 17:27:07 +0100

    鹏哥C语言
    练习1

---
+++ test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("%zu\n",sizeof(char));
+    printf("%zu\n",sizeof(short));
+    printf("%zu\n",sizeof(int)); 
+    printf("%zu\n",sizeof(long));
+    return 0;
+}
